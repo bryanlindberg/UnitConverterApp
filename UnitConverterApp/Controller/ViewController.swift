7ef898c1290898a886a738a8fc473715b69d8caa
Bryan Lindberg <bryanlindberg331@gmail.com>
2025-03-31 13:54:25 -0400

    3/31/25

--- UnitConverterApp/Controller/ViewController.swift
+++ UnitConverterApp/Controller/ViewController.swift
@@ -9,15 +9,125 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var valueTextBox: UITextField!
-    @IBOutlet weak var pickerView: UIPickerView!
     @IBOutlet weak var displayLabel: UILabel!
+    @IBOutlet weak var conversionPickerView: UIPickerView!
+    @IBOutlet weak var reverseSwitch: UISwitch!
     
+    let converter = ConverterModel()
+    //pickerview options
+    let conversionType = ["Length (Feet<->Miles)",
+                          "Length (Feet<->Meters)",
+                          "Length (Meters<->Miles)",
+                          "Weight (Pounds<->Kilograms)",
+                          "Weight (Pounds<->Ounces)",
+                          "Weight (Kilograms<->Ounces)",
+                          "Temp (Fahrenheit<->Celsius)",
+                          "Temp (Fahrenheit<->Kelvin)",
+                          "Temp (Kelvin<->Celsius)",
+                          "Speed (mph<->km/h)",
+                          "Speed (mph<->m/s)",
+                          "Speed (km/h<->m/s)"]
+    //dictionary to interpret the values from the pickerview input
+    let unitDictionary: [String: (String, String)] = [
+        "Length (Feet<->Miles)": ("feet", "miles"),
+        "Length (Feet<->Meters)": ("feet", "meters"),
+        "Length (Meters<->Miles)": ("meters", "miles"),
+        "Weight (Pounds<->Kilograms)": ("pounds", "kilograms"),
+        "Weight (Pounds<->Ounces)": ("pounds", "ounces"),
+        "Weight (Kilograms<->Ounces)": ("kilograms", "ounces"),
+        "Temp (Fahrenheit<->Celsius)": ("fahrenheit", "celsius"),
+        "Temp (Fahrenheit<->Kelvin)": ("fahrenheit", "kelvin"),
+        "Temp (Kelvin<->Celsius)": ("kelvin", "celsius"),
+        "Speed (mph<->km/h)": ("mph", "km/h"),
+        "Speed (mph<->m/s)": ("mph", "m/s"),
+        "Speed (km/h<->m/s)": ("km/h", "m/s")
+    ]
     
+    //instantiation of the default value
+    var selectedConversion = "Length (Feet<->Miles)"
+   
     
     override func viewDidLoad() {
+        displayLabel.isHidden = true
+        reverseSwitch.isOn = false
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        conversionPickerView.delegate = self
+        conversionPickerView.dataSource = self
     }
 
-
+    
+    
+    @IBAction func resetButton(_ sender: UIButton) {
+        valueTextBox.text = ""
+        displayLabel.text = ""
+        reverseSwitch.isOn = false
+    }
+    
+    
+    @IBAction func convertButton(_ sender: UIButton) {
+        displayLabel.isHidden = false
+        //error handling + unit assignment
+        guard let enteredValue = Double(valueTextBox.text ?? "") else {
+            displayLabel.text = "Invalid Input"
+            return
+        }
+        guard var units = unitDictionary[selectedConversion] else {
+            displayLabel.text = "Error"
+            return
+        }
+        
+        //handles the reverse button being pressed with a simple swapping problem
+        let isReversed = reverseSwitch.isOn
+        if isReversed {
+            let temp = units.0
+            units.0 = units.1
+            units.1 = temp
+        }
+        //calls the method to choose a conversion method
+        let convertedValue = convertValue(value: enteredValue, startingUnit: units.0, convertingTo: units.1)
+        displayLabel.text = String(format: "%.2f", convertedValue)
+        
+    }
+    //sends the value to the proper conversion method
+    func convertValue(value: Double, startingUnit: String, convertingTo endingValue: String) -> Double {
+        if ["feet", "meters", "miles"].contains(startingUnit) {
+            return converter.convertLength(value: value, startingUnit: startingUnit, convertingTo: endingValue)
+        }
+        else if ["celsius", "fahrenheit", "kelvin"].contains(startingUnit) {
+            return converter.convertTemp(value: value, startingUnit: startingUnit, convertingTo: endingValue)
+        }
+        else if ["pounds", "kilograms", "ounces"].contains(startingUnit) {
+            return converter.convertWeight(value: value, startingUnit: startingUnit, convertingTo: endingValue)
+        }
+        else if ["km/h", "mph", "m/s"].contains(startingUnit) {
+            return converter.convertSpeed(value: value, startingUnit: startingUnit, convertingTo: endingValue)
+        }
+        else {
+            return value
+        }
+    }
+    
+    
 }
+
+//I don't know why this is a separate class, I just saw it in a tutorial video and it's the only thing that didn't cause errors.
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return conversionType.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return conversionType[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        selectedConversion = conversionType[row]
+    }
+    
+}
+
+
--- UnitConverterApp/Model/ConverterModel.swift
+++ UnitConverterApp/Model/ConverterModel.swift
@@ -6,3 +6,103 @@
 //
 
 import Foundation
+
+
+struct ConverterModel {
+    func convertLength(value: Double, startingUnit: String, convertingTo: String) -> Double {
+        if startingUnit == "feet" && convertingTo == "miles" {
+            return value / 5280
+        }
+        else if startingUnit == "miles" && convertingTo == "feet" {
+            return value * 5280
+        }
+        else if startingUnit == "feet" && convertingTo == "meters" {
+            return value / 3.28084
+        }
+        else if startingUnit == "meters" && convertingTo == "feet" {
+            return value * 3.28084
+        }
+        else if startingUnit == "meters" && convertingTo == "miles" {
+            return value / 1609.34
+        }
+        else if startingUnit == "miles" && convertingTo == "meters" {
+            return value * 1609.34
+        }
+        else {
+            return value
+        }
+    }
+    
+    func convertWeight(value: Double, startingUnit: String, convertingTo: String) -> Double {
+        if startingUnit == "pounds" && convertingTo == "kilograms" {
+            return value / 2.20462
+        }
+        else if startingUnit == "kilograms" && convertingTo == "pounds" {
+            return value * 2.20462
+        }
+        else if startingUnit == "pounds" && convertingTo == "ounces" {
+            return value * 16
+        }
+        else if startingUnit == "ounces" && convertingTo == "pounds" {
+            return value / 16
+        }
+        else if startingUnit == "kilograms" && convertingTo == "ounces" {
+            return value * 35.274
+        }
+        else if startingUnit == "ounces" && convertingTo == "kilograms" {
+            return value / 35.274
+        }
+        else {
+            return value
+        }
+    }
+    
+    func convertTemp(value: Double, startingUnit: String, convertingTo: String) -> Double {
+        if startingUnit == "fahrenheit" && convertingTo == "celsius" {
+            return (value - 32) * 5 / 9
+        }
+        else if startingUnit == "celsius" && convertingTo == "fahrenheit" {
+            return value * 9 / 5 + 32
+        }
+        else if startingUnit == "fahrenheit" && convertingTo == "kelvin" {
+            return (value - 32) * 5 / 9 + 273.15
+        }
+        else if startingUnit == "kelvin" && convertingTo == "fahrenheit" {
+            return (value - 273.15) * 9 / 5 + 32
+        }
+        else if startingUnit == "kelvin" && convertingTo == "celsius" {
+            return value - 273.15
+        }
+        else if startingUnit == "celsius" && convertingTo == "kelvin" {
+            return value + 273.15
+        }
+        else {
+            return value
+        }
+        
+    }
+    
+    func convertSpeed(value: Double, startingUnit: String, convertingTo: String) -> Double {
+        if startingUnit == "mph" && convertingTo == "km/h" {
+            return value * 1.60934
+        }
+        else if startingUnit == "km/h" && convertingTo == "mph" {
+            return value * 0.621371
+        }
+        else if startingUnit == "mph" && convertingTo == "m/s" {
+            return value * 0.44704
+        }
+        else if startingUnit == "m/s" && convertingTo == "mph" {
+            return value * 2.23694
+        }
+        else if startingUnit == "km/h" && convertingTo == "m/s" {
+            return value * 0.277778
+        }
+        else if startingUnit == "m/s" && convertingTo == "km/h" {
+            return value * 3.6
+        }
+        else {
+            return value
+        }
+    }
+}
